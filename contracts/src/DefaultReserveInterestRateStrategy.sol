0bc93d0f0e4a7e070450dba0bf43fbcced83ee4b
locgiang <locbgiang@gmail.com>
2025-12-02 23:14:23 -0800

    Pool.sol PoolAddressesProvider.sol DefaultReserveInterestRate.sol

--- contracts/src/DefaultReserveInterestRateStrategy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-contract DefaultReserveInterestRateStrategy {
-    function calculateInterestRates(DataTypes.CalculateInterestRatesParams memory params) external view returns(uint256, uint256) {
-        // calculate interest rates logic
-    }
-}
--- contracts/src/PoolConfigurator.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-contract PoolConfigurator {
-    function initReserves(ConfiguratorInputTypes.InitReserveInput[] calldata input) external;
-    function configureReserveAsCollateral(address asset, uint256 ltv, uint256 liquidationThreshold, uint256 liquidationBonus) external;
-    function setReserveBorrowing(address asset, bool enabled) external;
-}
