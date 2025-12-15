744c45a137a45b79180230246d8f91724754ab75
Loc Giang <locbgiang@gmail.com>
2025-12-15 13:01:38 -0800

    fixed PriceOracle.sol

--- contracts/src/contracts/protocol/oracle/PriceOracle.sol
+++ contracts/src/contracts/protocol/oracle/PriceOracle.sol
@@ -7,20 +7,41 @@ pragma solidity ^0.8.10;
  */
 
 contract PriceOracle {
+    // errors
+    error PriceNotSet();
+    error StalePrice();
+    error InvalidPrice();
+    error NotOwner();
+    error ZeroAddress();
+
     address public owner;
 
-    // asset => price in USD (18 decimals)
-    // example: USDC = 1e18 ($1.00), ETH = 2000e18 ($2000)
-    mapping(address => uint256) public prices;
+    mapping(address => address)  public assetPriceFeed;
 
-    event PriceUpdated(address indexed asset, uint256 price);
+    mapping(address => uint256) public manualPrices;
+
+    mapping(address => bool) public useChainlink;
+
+    uint256 public constant STALENESS_THRESHOLD = 1 hours;
+
+    uint256 public constant PRICE_DECIMALS = 18;
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
 
     constructor() {
         owner = msg.sender;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        _;
+    function setAssetPriceFeed(address asset, address priceFeed) external onlyOwner {
+        if (asset == address(0)) revert ZeroAddress();
+        if (priceFeed == address(0)) revert ZeroAddress();
+
+        assetPriceFeeds[asset] = priceFeed;
+        useChainlink[asset] = true;
+
+        emit PriceFeedSet(asset, priceFeed);
     }
 }
